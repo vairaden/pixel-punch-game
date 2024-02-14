5eb10f49324f5faa809bce0d8e443ef57fba186d
Marina Golovanova <gma18@list.ru>
2024-02-15 01:44:10 +0300

    PIX-16 Refactor

--- packages/client/src/app/App/App.tsx
+++ packages/client/src/app/App/App.tsx
@@ -1,15 +1,11 @@
 import { HomePage } from '@/pages/HomePage';
 import '../style/root.scss';
-import { LoginPage } from '@/pages/LoginPage';
-import { RegistrationPage } from '@/pages/RegistrationPage';
 
 function App() {
   // TODO: Роутер
   return (
     <div data-testid="app" id="app">
-      {/* <HomePage /> */}
-      <LoginPage />
-      {/* <RegistrationPage /> */}
+      <HomePage />
     </div>
   );
 }
