765eb1b79f96933fb512294062b016d6a83102f3
Boris Avgustinovich <74373178+vairaden@users.noreply.github.com>
2024-02-19 19:23:49 +0300

    starting screen styling

--- packages/client/src/pages/StartPage/StartPage.tsx
+++ packages/client/src/pages/StartPage/StartPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Paper, Typography } from '@mui/material';
 import { FC, useCallback, useRef, useState } from 'react';
 
 interface Props {
@@ -6,28 +6,35 @@ interface Props {
   countdown: number;
 }
 
-export const StartPage: FC<Props> = ({countDownCallback, countdown}) => {
- const [remainingTime, setRemainingTime] = useState(countdown);
- const timeRef = useRef(countdown);
+export const StartPage: FC<Props> = ({ countDownCallback, countdown }) => {
+  const [remainingTime, setRemainingTime] = useState(countdown);
+  const [isCountdown, setIsCountdown] = useState(false);
+  const timeRef = useRef(countdown);
 
- const startTimer = useCallback(() => {
-   const interval = setInterval(() => {
-     setRemainingTime((prev) => prev - 1);
-     timeRef.current -= 1;
+  const startTimer = useCallback(() => {
+    setIsCountdown(true);
 
-     if (timeRef.current === 0) {
-       clearInterval(interval);
-       countDownCallback();
-     }
-   }, 1000);
- }, []);
+    const interval = setInterval(() => {
+      setRemainingTime((prev) => prev - 1);
+      timeRef.current -= 1;
+
+      if (timeRef.current === 0) {
+        clearInterval(interval);
+        countDownCallback();
+      }
+    }, 1000);
+  }, []);
 
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        {remainingTime}
-        <Button onClick={startTimer}>Начать</Button>
+        <Paper sx={{ display: 'flex', flexDirection: 'column', padding: 8 }}>
+          <Typography sx={{textAlign: 'center', mb: 4}} variant="h3">
+            {isCountdown ? remainingTime : 'Нажмите на конпку'}
+          </Typography>
+          <Button disabled={isCountdown} variant="contained" onClick={startTimer}>Начать</Button>
+        </Paper>
       </Box>
     </Container>
   );
-}
+};
