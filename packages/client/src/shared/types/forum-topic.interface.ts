d821861e6a0b9f0c7c039bb00e3efd0b04db36f7
Vladimir Borshev <drop9dead@gmail.com>
2024-02-25 14:21:18 +0300

    Put the author of the topic in a separate type

--- packages/client/src/shared/types/forum-topic.interface.ts
+++ packages/client/src/shared/types/forum-topic.interface.ts
@@ -1,11 +1,16 @@
 import { IUser } from './auth.interface';
 
+export type IForumTopicAuthor = Pick<
+  IUser,
+  'first_name' | 'second_name' | 'avatar'
+>;
+
 export type IForumTopic = {
   id: number;
   title: string;
   body?: string;
   createAt: string;
-  author: Pick<IUser, 'first_name' | 'second_name' | 'avatar'>;
+  author: IForumTopicAuthor;
   commentsCount: number;
 };
 
@@ -13,5 +18,5 @@ export type IForumTopicComment = {
   id: number;
   text: string;
   createAt: string;
-  author: Pick<IUser, 'first_name' | 'second_name' | 'avatar'>;
+  author: IForumTopicAuthor;
 };
