1cb2cd8c322cdcf82d0c43848ce3b44185ba5e7d
Никита <pravoepredserdie@gmail.com>
2024-03-17 13:23:52 +0300

    Настройка vite, express, client для SSR

---
+++ packages/client/src/app/MockPage.tsx
@@ -0,0 +1,11 @@
+// Удалить после перехода на App
+import { Box, Typography } from '@mui/material';
+
+export const MockPage = () => {
+  return (
+    <Box>
+      <Typography>Hello there!</Typography>
+      <Typography>This page was rendered on server!</Typography>
+    </Box>
+  );
+};
--- packages/client/src/app/index.tsx
+++ packages/client/src/app/index.tsx
@@ -1,29 +1,39 @@
-import { ThemeProvider } from '@emotion/react';
+// import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/App';
-import { CssBaseline } from '@mui/material';
-import theme from '@/app/theme';
-import { Provider } from 'react-redux';
-import { store } from '@/shared/store';
-import {
-  registerServiceWorker,
-  unregisterServiceWorker,
-} from '@/shared/utils/serviceWorker';
+// import App from './App/App';
+// import { CssBaseline } from '@mui/material';
+// import theme from '@/app/theme';
+// import { Provider } from 'react-redux';
+// import { store } from '@/shared/store';
+// import {
+//   registerServiceWorker,
+//   unregisterServiceWorker,
+// } from '@/shared/utils/serviceWorker';
 
-if (import.meta.env.PROD) {
-  registerServiceWorker();
-} else {
-  unregisterServiceWorker();
-}
+// if (import.meta.env.PROD) {
+//   registerServiceWorker();
+// } else {
+//   unregisterServiceWorker();
+// }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// ReactDOM.hydrateRoot(
+//   document.getElementById('root') as HTMLElement,
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <ThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <App />
+//       </ThemeProvider>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// Поменять на App
+import { MockPage } from './MockPage';
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <MockPage />
   </React.StrictMode>
 );
