222989931d33b67f8b20f199c1d9e55169a2ebad
Vladimir Borshev <drop9dead@gmail.com>
2024-04-06 11:06:17 +0300

    Del unusable logs and change variables name

--- packages/server/src/controllers/topicReaction.controller.ts
+++ packages/server/src/controllers/topicReaction.controller.ts
@@ -3,25 +3,23 @@ import { TopicReaction, UserReaction } from '../models';
 
 class TopicReactoinController {
   async updateReaction(req: Request, res: Response) {
-    const { topic_id: topicId } = req.params;
-    const { reaction_name: reactionName } = req.body;
+    const { topic_id } = req.params;
+    const { reaction_name } = req.body;
 
-    if (!topicId) {
+    if (!topic_id) {
       res.status(400).send('Bad request');
       return;
     }
 
     const userId = res.locals.user.id;
 
-    console.log(topicId, reactionName, userId);
-
     try {
       // Пользователь убрал реакцию с поста
-      if (!reactionName) {
+      if (!reaction_name) {
         TopicReaction.destroy({
           where: {
             user_id: userId,
-            topic_id: topicId,
+            topic_id,
           },
         });
         res.send('Deleted');
@@ -31,14 +29,14 @@ class TopicReactoinController {
       const [reaction] = await TopicReaction.findOrCreate({
         where: {
           user_id: userId,
-          topic_id: topicId,
+          topic_id,
         },
         defaults: {
-          reaction_name: reactionName,
+          reaction_name,
         },
       });
 
-      reaction.update({ reaction_name: reactionName });
+      reaction.update({ reaction_name });
       res.send('Updated');
     } catch (e) {
       res.status(500).send(e);
